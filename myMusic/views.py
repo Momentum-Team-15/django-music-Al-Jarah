6bf4ef03a3353a1ce53e47d0e5219561c279ae55
Elijah Brown <elibrown0731@gmail.com>
2022-10-21 14:45:46 -0400

    added a pk for album detail page

--- myMusic/views.py
+++ myMusic/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from .models import Album
 
 
 # Create your views here.
 def index(request):
     return render(request, 'myMusic/index.html')
+
+
+def album_detail(request, pk):
+    album = Album.objects.get(pk=pk)
+    return render(request, 'music/album_detail.html', {'album': album})

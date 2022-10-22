88b46c91e0622a0c3773f7d9374ffa235e2c3d02
Elijah Brown <elibrown0731@gmail.com>
2022-10-22 19:45:41 -0400

    added create album and edit album view

--- myMusic/views.py
+++ myMusic/views.py
@@ -1,12 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Album
+from .forms import AlbumForm
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'myMusic/index.html')
+    albums = Album.objects.all()
+    return render(request, 'myMusic/index.html', {'albums': albums})
 
 
 def album_detail(request, pk):
     album = Album.objects.get(pk=pk)
-    return render(request, 'music/album_detail.html', {'album': album})
+    return render(request, 'myMusic/album_detail.html', {'album': album})
+
+
+def create_album(request):
+    if request.method == 'POST':
+        form = AlbumForm(request.POST)
+        if form.is_valid():
+            album=form.save()
+            return redirect("home")
+    else:
+        form = AlbumForm()
+    return render(request, 'myMusic/create_album.html', {'form': form})
+
+
+def album_edit(request, pk):
+    album = get_object_or_404(Album, pk=pk)
+    if request.method == "POST":
+        form = AlbumForm(request.POST)
+        if form.is_valid():
+            album = form.save()
+            return redirect('album_detail', pk=album.pk)
+    else:
+        form = AlbumForm(instance=Album)
+    return render(request, 'myMusic/album_edit.html', {'form': form})

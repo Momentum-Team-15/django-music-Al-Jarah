13ced3a80f4a6c50755608b109f433780cd4a37f
Elijah Brown <elibrown0731@gmail.com>
2022-10-24 13:44:16 -0400

    cleaning up  code

--- myMusic/models.py
+++ myMusic/models.py
@@ -7,13 +7,12 @@ class User(AbstractUser):
 
 
 class Album(models.Model):
+    user = models.ForeignKey('User', on_delete=models.CASCADE, blank=True, null=True)
     title = models.CharField(max_length=200)
     artist = models.ForeignKey('Artist', on_delete=models.CASCADE, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    # image = models.ImageField(upload_to='images/')
     cover = models.ImageField(upload_to='images/', blank=True, null=True)
-    user = models.ForeignKey('User', on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
         return f"{self.title} by {self.artist}"

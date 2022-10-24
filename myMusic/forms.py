55f2a0bb8ea8497a93950a99f7d85824e0dc09a0
Elijah Brown <elibrown0731@gmail.com>
2022-10-24 13:44:02 -0400

    cleaing up code

--- myMusic/forms.py
+++ myMusic/forms.py
@@ -5,5 +5,5 @@ class AlbumForm(forms.ModelForm):
 
     class Meta:
         model = Album
-        #this is the bit that is spicy, you can add as many fields as you want
+        # this is the bit that is spicy, you can add as many fields as you want
         fields = ['cover', 'title', 'artist']
